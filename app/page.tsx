584ae5132d583e1565e1715326b79629f10e02e4
Akash-develop <akash.p02.dev@gmail.com>
2026-03-03 19:30:57 +0530

    bug-fix

--- app/page.tsx
+++ app/page.tsx
@@ -221,12 +221,12 @@ export default function Home() {
             </div>
           </div>
 
-       
+          {loading && (
             <div className="text-center py-8">
               <div className="inline-block animate-spin rounded-full h-8 w-8 border-b-2 border-blue-600"></div>
               <p className="mt-2 text-gray-600">Loading...</p>
             </div>
-
+          )}
 
           {books.length > 0 && !loading && (
             <>
